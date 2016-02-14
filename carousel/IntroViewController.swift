d19952044099aaf09e6a38d32c7260da407bda49
Emma Guo <emmahongguo@gmail.com>
2016-02-14 10:36:04 -0800

    Create intro screen

---
+++ carousel/IntroViewController.swift
@@ -0,0 +1,39 @@
+//
+//  IntroViewController.swift
+//  carousel
+//
+//  Created by Emma Guo on 2/13/16.
+//  Copyright © 2016 emmaguo. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var introImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        scrollView.contentSize = introImageView.image!.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
