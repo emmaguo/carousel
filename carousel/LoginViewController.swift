ec554ee40df2b8a852337061c5fb60d57fa5231b
Emma Guo <emmahongguo@gmail.com>
2016-02-14 11:08:25 -0800

    Create signin screen

--- carousel/LoginViewController.swift
+++ carousel/LoginViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var buttonParentView: UIView!
+    
+    var buttonInitialY: CGFloat!
+    var buttonOffset: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        scrollView.contentSize = scrollView.frame.size
+        scrollView.contentInset.bottom = 100
+        buttonInitialY = buttonParentView.frame.origin.y
+        buttonOffset = -140
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        // Move the button up above keyboard
+        buttonParentView.frame.origin.y = buttonInitialY + buttonOffset
+        // Scroll the scrollview up
+        scrollView.contentOffset.y = scrollView.contentInset.bottom
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        buttonParentView.frame.origin.y = buttonInitialY
+        scrollView.contentOffset.y = 0
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        // This method is called as the user scrolls
     }
     
 
