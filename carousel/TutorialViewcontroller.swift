a5f1cb949bfc63ac0856e4472648ee1266dd28f2
Emma Guo <emmahongguo@gmail.com>
2016-02-14 17:02:35 -0800

    Create tutorial screens

--- carousel/TutorialViewcontroller.swift
+++ carousel/TutorialViewcontroller.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class TutorialViewcontroller: UIViewController {
+class TutorialViewcontroller: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var backupButtonView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        scrollView.contentSize = CGSize(
+            width: imageView.frame.width * 4,
+            height: imageView.frame.height)
+        backupButtonView.alpha = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +31,21 @@ class TutorialViewcontroller: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        // Get the current page based on the scroll offset
+        let page : Int = Int(round(scrollView.contentOffset.x / 320))
+        
+        // Set the current page, so the dots will update
+        pageControl.currentPage = page
+        
+        if page == 3 {
+            UIView.animateWithDuration(0.4, animations: {
+                self.backupButtonView.alpha = 1
+            })
+        } else {
+            backupButtonView.alpha = 0
+        }
+    }
 
     /*
     // MARK: - Navigation
